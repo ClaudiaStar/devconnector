f0df9003d793be388b3086c6e3adef9484f9e47e
Claudia Estrada <cgestrada518@gmail.com>
2020-01-08 10:53:41 -0800

    create the User mongoose model

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  avatar: { type: String },
+  date: { type: date, default: date.now() }
+});
+
+// Mongoose.model takes 2 inputs, model name & actual model
+module.exports = User = mongoose.model("user", UserSchema);
